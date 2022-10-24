717ed2f4db874f72d5d7f5b902910cdf34e5a3cc
phamtanhoang <88889770+phamtanhoang@users.noreply.github.com>
2022-10-24 17:52:33 +0700

    first commit

---
+++ saleapp/index.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+from saleapp import app
+import utils
+
+@app.route("/")
+def home():
+    cates = utils.load_categories()
+    return render_template('index.html', categories=cates)
+
+@app.route("/products")
+def product_list():
+    cate_id = request.args.get("category_id")
+    kw = request.args.get("kw")
+    from_price = request.args.get("from_price")
+    to_price = request.args.get("to_price")
+    products = utils.load_products(cate_id=cate_id, kw=kw, from_price=from_price, to_price=to_price)
+    return render_template('products.html', products=products)
+
+@app.route("/products/<int:product_id>")
+def product_detail(product_id):
+    product = utils.get_product_by_id(product_id)
+    return render_template('product_detail.html', product=product)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ saleapp/utils.py
@@ -0,0 +1,27 @@
+import json, os
+from saleapp import app
+
+def read_json(path):
+    with open(path, "r") as f:
+        return json.load(f)
+
+def load_categories():
+    return read_json(os.path.join(app.root_path, 'data/categories.json'))
+
+def load_products(cate_id=None,kw=None,from_price=None,to_price=None):
+    products = read_json(os.path.join(app.root_path, 'data/products.json'))
+    if cate_id:
+        products = [p for p in products if p['category_id'] == int(cate_id)]
+    if kw:
+        products = [p for p in products if p['name'].lower().find(kw.lower()) >= 0]
+    if from_price:
+        products = [p for p in products if p['price'] >= int(from_price)]
+    if to_price:
+        products = [p for p in products if p['price'] <= int(to_price)]
+    return products
+
+def get_product_by_id(product_id):
+    products = read_json(os.path.join(app.root_path, 'data/products.json'))
+    for p in products:
+        if p["id"] == product_id:
+            return p
